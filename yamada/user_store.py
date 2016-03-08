01ba9f58742a80b537a93c2c463ed2b5da6f49e8
Keichi Takahashi <keichi.t@me.com>
2016-03-08 17:34:52 +0900

    UserStoreの例外処理とエラーメッセージを拡充

--- yamada/user_store.py
+++ yamada/user_store.py
@@ -1,6 +1,6 @@
 import logging
 
-from yaml import load
+from yaml import load, YAMLError
 
 
 class UserStore(object):
@@ -26,35 +26,52 @@ class UserStore(object):
         except IOError:
             self._logger.error("Could not open %s", self.user_role_file)
             return
+        except YAMLError:
+            self._logger.warning("Error while parsing %s",
+                                 self.user_role_file)
+            return
 
-        if "users" in data:
-            self._logger.info("Reading user data")
-            self._store_user_data(data["users"])
         if "roles" in data:
             self._logger.info("Reading role data")
             self._store_role_data(data["roles"])
+        if "users" in data:
+            self._logger.info("Reading user data")
+            self._store_user_data(data["users"])
 
     def _store_user_data(self, users):
         for user in users:
             if not self._validate_user_keys(user):
-                return
+                continue
+
             name = user["name"]
             password = user["password"]
             role = user["role"]
             u = User(name, password, role)
-            self.users.update({name: u})
+
+            if role not in self.roles:
+                self._logger.warning("Unknown role %s for user %s",
+                                     role, name)
+                continue
+            if name in self.users:
+                self._logger.warning("Duplicate user name %s", name)
+                continue
+            self.users[name] = u
 
     def _store_role_data(self, roles):
         for role in roles:
             if not self._validate_role_keys(role):
-                return
+                continue
+
             name = role["name"]
             allowed_hosts = []
             for host in role["allowed_hosts"]:
                 allowed_hosts.append(host)
 
             r = Role(name, allowed_hosts)
-            self.roles.update({name: r})
+            if name in self.roles:
+                self._logger.warning("Duplicate role name %s", name)
+                continue
+            self.roles[name] = r
 
     def _validate_user_keys(self, user):
         return "name" in user and "password" in user and "role" in user
